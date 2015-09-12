ab84123319d06c899ad6df88e3739138e6d2cc6d
Erik Warringer <erikwarringer@gmail.com>
2015-09-12 18:34:47 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606200404) do
+ActiveRecord::Schema.define(version: 20150910213752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendees", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "meetup_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "attendees", ["meetup_id"], name: "index_attendees_on_meetup_id", using: :btree
+  add_index "attendees", ["user_id", "meetup_id"], name: "index_attendees_on_user_id_and_meetup_id", unique: true, using: :btree
+  add_index "attendees", ["user_id"], name: "index_attendees_on_user_id", using: :btree
+
+  create_table "meetups", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.string   "location",    null: false
+    t.text     "description", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",   null: false
